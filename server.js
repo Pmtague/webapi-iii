a2edf61df4f6f4cdcf4e35259fba18ad6d489947
Penny Tague <pmtague9@gmail.com>
2019-09-05 07:37:24 -0700

    Added three user routes

--- server.js
+++ server.js
@@ -4,23 +4,21 @@ const server = express();
 
 server.use(express.json());
 
-server.use('/add-product', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>The "Add Product" Page</h1>');
-});
 server.use('/', (req, res, next) => {
   console.log('In another middleware');
   res.send('<h1>Hello from Express</h1>');
 });
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
-// function logger(req, res, next) {
-//   console.log(req.params)
-// };
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${ req.method } to ${ req.originalUrl } from ${ req.get('Origin') }`
+  );
+  next();
+};
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,24 +1,60 @@
 const express = require('express');
+const userDB = require('./userDb.js');
+const postDB = require('../posts/postDb.js')
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
+	const newUser = req.body;
 
+	userDB.insert(newUser)
+		.then(result => {
+			res.status(201).json(result)
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'Error saving to the database' })
+		})
 });
 
 router.post('/:id/posts', (req, res) => {
+	const newPost = req.body;
+	const postId = req.params.id;
 
+	userDB.getById(postId)
+		.then(user => {
+			postDB.insert(newPost)
+		})
+		.then(result => {
+			res.status(201).json(result)
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'Error saving to the database' })
+		})
 });
 
 router.get('/', (req, res) => {
-
+	userDB.get()
+		.then(result => {
+			res.status(200).json(result)
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'User info not found' })
+		})
 });
 
 router.get('/:id', (req, res) => {
+	const userId = req.params.id;
 
+	userDB.getById(userId)
+		.then(result => {
+			res.status(200).json(result)
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'User info not found' })
+		})
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
 
 });
 
